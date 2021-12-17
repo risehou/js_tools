20bf88948c9a1c1a1883e20b5e45ddf2e4303d99
risehou <38919094+risehou@users.noreply.github.com>
2021-12-17 19:34:25 +0900

    Update checkInput.js

--- checkInput.js
+++ checkInput.js
@@ -6,23 +6,18 @@ function zenkaku2hankaku($this) {
         var str = $this.value;
         var id = $this.id;
 
+        if (id == "TEL") {
+            str = str.replaceAll(/\D/g, "");
+        } else if (id == "YUBIN") {
+            str = str.replaceAll(/\D/g, "");
+        } else{
+            str = str.replaceAll(" ", "");
+            str = str.replaceAll("　", "");
+        }
+        
         var out = str.replace(/[！-～]/g, function (s) {
             return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
         });
  
-        if (id == "TEL") {
-            out = out.replaceAll("-", "");
-            out = out.replaceAll("/", "");
-            out = str.replaceAll(" ", "");
-            out = out.replaceAll("　", "");
-        } else if (id == "YUBIN") {
-            out = out.replaceAll("-", "");
-            out = out.replaceAll("/", "");
-            out = out.replaceAll(" ", "");
-            out = out.replaceAll("　", ""); 
-        } else{
-            out = str.replaceAll(" ", "");
-            out = str.replaceAll("　", "");
-        }
         $this.value = out;
 }
